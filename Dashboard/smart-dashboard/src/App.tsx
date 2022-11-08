a790755d9b55ffc4b50b0e65de312e435d93a26e
Denmads <43139465+Denmads@users.noreply.github.com>
2022-11-08 11:10:36 +0100

    React App Initialization

---
+++ Dashboard/smart-dashboard/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Paper
+          elevation={3}
+          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+        >
+          <Typography color="primary.dark" variant="h1">
+            Smart Dashboard
+          </Typography>
+        </Paper>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
