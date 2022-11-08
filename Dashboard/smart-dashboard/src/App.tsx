873329afd8c56673b5f337b5f9b88f42c4a81a7d
Denmads <43139465+Denmads@users.noreply.github.com>
2022-11-08 12:47:08 +0100

    Added routes and theme

--- Dashboard/smart-dashboard/src/App.tsx
+++ Dashboard/smart-dashboard/src/App.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import './App.css';
-import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { routes as appRoutes } from "./routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#93aa5e',
+      },
+      secondary: {
+        main: '#66c5ad',
+      }
+    }
+  });
+  
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Paper
-          elevation={3}
-          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
-        >
-          <Typography color="primary.dark" variant="h1">
-            Smart Dashboard
-          </Typography>
-        </Paper>
+      <Box height="100vh" display="flex" flexDirection="column" >
+        <Router>
+          <Routes>
+            {
+              appRoutes.map(route => (
+                <Route 
+                  key={route.key}
+                  path={route.path}
+                  element={<route.component />}
+                />
+              ))
+            }
+          </Routes>
+        </Router>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 }
 
