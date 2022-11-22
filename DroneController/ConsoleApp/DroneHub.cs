2537999fa563b50013eb3a87cdcef9fa46792044
Bjarne Kjær <bjarne.kjaer@lego.com>
2022-11-22 14:54:19 +0100

    DroneHub and models

---
+++ DroneController/ConsoleApp/DroneHub.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp
+{
+    public class DroneHub
+    {
+        private readonly string droneHubBaseUrl = "https://localhost:5000/";
+
+        public bool Post(string parameters, string payload)
+        {
+            try
+            {
+                using HttpClient client = new();
+                client.BaseAddress = new Uri(droneHubBaseUrl);
+                HttpResponseMessage response = client.PostAsync(parameters, new StringContent(payload)).Result;
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to reach {droneHubBaseUrl}: {ex.Message}");
+                return false;
+            }
+        }
+
+        public string Get(string parameters)
+        {
+            try
+            {
+                using HttpClient client = new();
+                client.BaseAddress = new Uri(droneHubBaseUrl);
+                HttpResponseMessage response = client.GetAsync(parameters).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to reach {droneHubBaseUrl}: {ex.Message}");
+                return "";
+            }
+        }
+    }
+}
