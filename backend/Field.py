038b00a6f77db7da24e16f52f52b180f1bbd5444
Oblivoin <pertl18@student.sdu.dk>
2022-11-22 13:06:57 +0100

    create

--- backend/Field.py
+++ backend/Field.py
@@ -1,25 +1,25 @@
 import json
 
 class Field:
-    def __init__(self, hieght, width, name, id=-1):
-        self.hieght = hieght
+    def __init__(self, height, width, name, id=-1):
+        self.height = height
         self.width = width
         self.name = name
         self.id = id
 
     def __str__(self):
-        return f"({self.id}){self.name}({self.hieght} by {self.width})"
+        return f"({self.id}){self.name}({self.height} by {self.width})"
 
     def to_json(self):
-        return {"id":self.id, "name":f"{self.name}", "width":self.width, "hieght":self.hieght}
+        return {"id":self.id, "name":f"{self.name}", "width":self.width, "height":self.height}
 
     def to_json_with_drones(self, hub):
         droneslist = []
         for drone in hub.drones:
             droneslist.append(drone.to_json())
             
-        j = {"id":self.id, "name":f"{self.name}", "width":self.width, "hieght":self.hieght, "drones": json.dumps(droneslist)}
+        j = {"id":self.id, "name":f"{self.name}", "width":self.width, "height":self.height, "drones": json.dumps(droneslist)}
         return j
 
     def insert_into_db(self):
-        db.save(f"INSERT INTO ff (Width, height, FieldName)VALUES({self.width}, {self.hieght}, {self.name})")
+        db.save(f"INSERT INTO ff (Width, height, FieldName)VALUES({self.width}, {self.height}, {self.name})")
