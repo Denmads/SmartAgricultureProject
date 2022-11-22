d47fc0d7d9a1cfa09e69f8ff91fb83e6c9d2215e
Oblivoin <pertl18@student.sdu.dk>
2022-11-22 11:18:26 +0100

    docker-compose not worling

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    connection = mysql.connector.connect(host='localhost',
+    connection = mysql.connector.connect(host='db',
                                          database='hub',
                                          user='root',
                                          password='password')
